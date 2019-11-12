1e70a871e62c8b8ba34f8adf963f431d0679bc52
Aurimas N <aurimas.navardauskas@outlook.com>
2019-11-12 10:19:00 +0200

    Lab 2 exercise 3

--- Allfiles/Mod02/Labfiles/Starter/Exercise 3/School/MainWindow.xaml.cs
+++ Allfiles/Mod02/Labfiles/Starter/Exercise 3/School/MainWindow.xaml.cs
@@ -7,7 +7,8 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using School.Data;
-
+using System.Data;
+using System.Data.Objects;
 // TODO: Exercise 3: Task 2a: Bring the System.Data and System.Data.Objects namespaces into scope
 
 namespace School
@@ -166,6 +167,28 @@ namespace School
         private void saveChanges_Click(object sender, RoutedEventArgs e)
         {
             // TODO: Exercise 3: Task 2b: Save the changes by calling the SaveChanges method of the schoolContext object
+            try
+            {
+                schoolContext.SaveChanges();
+                saveChanges.IsEnabled = false;
+            }
+            catch (OptimisticConcurrencyException)
+            {
+                schoolContext.Refresh(RefreshMode.ClientWins, schoolContext.Students);
+                schoolContext.SaveChanges();
+            }
+            catch (UpdateException uEx)
+            {
+                MessageBox.Show(uEx.InnerException.Message, "Error saving changes");
+                schoolContext.Refresh(RefreshMode.StoreWins, schoolContext.Students);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error saving changes");
+                schoolContext.Refresh(RefreshMode.ClientWins, schoolContext.Students);
+            }
+            
+            
             // TODO: Exercise 3: Task 3a: If an OptimisticConcurrencyException occurs then another user has changed the same students earlier, then overwrite their changes with the new data (see the lab instructions for details)
             // TODO: Exercise 3: Task 3b: If an UpdateException occurs then report the error to the user and rollback (see the lab instructions for details)
             // TODO: Exercise 3: Task 3c: If some other sort of error has occurs, report the error to the user and retain the data so the user can try again - the error may be transitory (see the lab instructions for details)
